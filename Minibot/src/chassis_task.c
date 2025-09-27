1b22bb9bfe1495443b70737e1c381e078469323c
vMiloo <milolireed@gmail.com>
2025-09-27 16:06:04 -0400

    Motor_Config_t

--- Minibot/src/chassis_task.c
+++ Minibot/src/chassis_task.c
@@ -14,6 +14,22 @@ float chassis_rad;
 void Chassis_Task_Init()
 {
     // Init chassis hardware
+        
+    Motor_Config_t chassis_w1 = {   // added(mr)
+        .can_bus = 1, // what can bus the motor is on
+        .speed_controller_id = 1, // identifier for each motor
+        .offset = 0, // Initial offset of the motor (used for encoder)
+        .control_mode = VELOCITY_CONTROL, // Control mode of the motor (setting velocity or setting position/angle)
+        .motor_reversal = MOTOR_REVERSAL_NORMAL, // Direction of the motor
+        .velocity_pid = // pid
+            {
+                .kp = 500.0f,
+                .kd = 0.0f,
+                .kf = 0.0f,
+                .output_limit = M2006_MAX_CURRENT, // m2006 is the motor
+            },
+    };
+    motor_w1 = DJI_Motor_Init(&chassis_w1, M2006); // Initializing motor
 }
 
 void Chassis_Ctrl_Loop()
