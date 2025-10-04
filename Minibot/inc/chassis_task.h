d02e6a5807421dfa4910e39119231c3b87c71c6b
AndrewZacharyLiu <87785681+AndrewZacharyLiu@users.noreply.github.com>
2025-10-04 15:40:24 -0400

    did stuff with initialize motors, set velocity

--- Minibot/inc/chassis_task.h
+++ Minibot/inc/chassis_task.h
@@ -5,4 +5,10 @@
 void Chassis_Task_Init(void);
 void Chassis_Ctrl_Loop(void);
 
+//Added by Andrew Liu
+//Constants for wheel dimension
+#define WHEEL_RADIUS 0.05
+#define WHEEL_DISTANCE 1
+#define MOTOR_COUNT 4
+
 #endif // CHASSIS_TASK_H
--- Minibot/src/chassis_task.c
+++ Minibot/src/chassis_task.c
@@ -2,31 +2,33 @@
 
 #include "robot.h"
 #include "remote.h"
-
+#include "motor.h"
+#include "dji_motor.h"
 extern Robot_State_t g_robot_state;
 extern Remote_t g_remote;
-
-float chassis_rad;
-
+//Added global variable to store motors
+DJI_Motor_Handle_t *omni_motors[MOTOR_COUNT];
 void Chassis_Task_Init()
 {
-    // Init chassis hardware
-        
-    Motor_Config_t chassis_w1 = {   // added(mr)
-        .can_bus = 1, // what can bus the motor is on
-        .speed_controller_id = 1, // identifier for each motor
-        .offset = 0, // Initial offset of the motor (used for encoder)
-        .control_mode = VELOCITY_CONTROL, // Control mode of the motor (setting velocity or setting position/angle)
-        .motor_reversal = MOTOR_REVERSAL_NORMAL, // Direction of the motor
-        .velocity_pid = // pid
-            {
-                .kp = 500.0f,
-                .kd = 0.0f,
-                .kf = 0.0f,
-                .output_limit = M2006_MAX_CURRENT, // m2006 is the motor
-            },
-    };
-    motor_w1 = DJI_Motor_Init(&chassis_w1, M2006); // Initializing motor
+    //intialize motors
+    // Added by Andrew Liu
+    for (int i = 1; i <= MOTOR_COUNT; i++) {
+        Motor_Config_t chassis_wi = {
+            .can_bus = 1, // what can bus the motor is on
+            .speed_controller_id = i, // identifier for each motor
+            .offset = 0, // Initial offset of the motor (used for encoder)
+            .control_mode = VELOCITY_CONTROL, // Control mode of the motor
+            .motor_reversal = MOTOR_REVERSAL_NORMAL, // Direction of the motor
+            .velocity_pid = // pid
+                {
+                    .kp = 500.0f,
+                    .kd = 0.0f,
+                    .kf = 0.0f,
+                    .output_limit = M2006_MAX_CURRENT, // m2006 is the motor
+                },
+        };
+        omni_motors[i] = DJI_Motor_Init(&chassis_wi, M2006); // Initializing motor
+    }
 }
 
 void Chassis_Ctrl_Loop()
@@ -36,20 +38,21 @@ void Chassis_Ctrl_Loop()
 
 }
 
-//Added helper function convert input into omni chassis movements
+//Andrew Liu: Added helper function convert input into omni chassis movements
 void update_wheelmovements_from_input() {
-        float wheel_radius = 1; // TODO: This is hardcoded constant. replace with actual
-        float d = 1; // TODO: This is hardcoded constant. replace with actual. d is distance between wheel
         float vx = g_robot_state.chassis.x_speed;
         float vy = g_robot_state.chassis.y_speed;
-
-        float theta = 0;
+        //TODO: what is theta?
+        float theta = g_robot_state.chassis.omega;
         float sintheta = sin(theta);
         float costheta = cos(theta);
-
-        //TODO: call set velocty functions to set each wheel movements
-        wheelmovements[0] = (sintheta *  vx + costheta *  vy + d * theta) / wheel_radius;
-        wheelmovements[1] = (-1 * costheta *  vx - sintheta *  vy + d * theta) / wheel_radius;
-        wheelmovements[2] = (sintheta *  vx - costheta *  vy + d * theta) / wheel_radius;
-        wheelmovements[3] = (costheta *  vx + sintheta *  vy+ d * theta) / wheel_radius;
+        float wheel_speeds[4] = {0.0f};
+        wheel_speeds[0] = (sintheta *  vx + costheta *  vy + WHEEL_DISTANCE * theta) / WHEEL_RADIUS;
+        wheel_speeds[1] = (-1 * costheta *  vx - sintheta *  vy + WHEEL_DISTANCE * theta) / WHEEL_RADIUS;
+        wheel_speeds[2] = (sintheta *  vx - costheta *  vy + WHEEL_DISTANCE * theta) / WHEEL_RADIUS;
+        wheel_speeds[3] = (costheta *  vx + sintheta *  vy+ WHEEL_DISTANCE * theta) / WHEEL_RADIUS;
+        for (int i = 0; i < MOTOR_COUNT; i++) {
+        DJI_Motor_Set_Velocity(omni_motors[i], wheel_speeds[i]);
+        }
+        
 }
